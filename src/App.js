afd6bb7626e91cea9e405772daac9f622c3bb2f3
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-11-24 14:09:12 +0530

    Added codes for react-counter app project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Button from "./components/Button"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  let resetCount = () => {
+    setCount(0);
+  }
+
+  return (
+    <div className="app">
+        <p>Count: {count}</p>
+        <div className="buttons">
+          <Button title={"Decrement"} action={decrementCount} />
+          <Button title={"Increment"} action={incrementCount} />
+          <Button title={"Reset"} action={resetCount} />
+        </div>
+      </div>
+  );
+}
+
+export default App;
